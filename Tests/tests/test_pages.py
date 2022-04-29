253ac7c45228f689d6c9a04830786c2ef5f6975f
cbjfan2009 <cbjfan2009@gmail.com>
2022-04-29 08:54:34 -0400

    committing for review.  still not functional using POM

--- Tests/tests/test_pages.py
+++ Tests/tests/test_pages.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import pytest
 from Tests.pages.mainPage import MainPage
-from Tests.pages.aboutPage import AboutPage
+# from Tests.pages.aboutPage import AboutPage
 
 
 # Chrome driver path
@@ -15,23 +15,22 @@ s = Service('C:\\Program Files (x86)\\chromedriver.exe')
 driver = webdriver.Chrome(service=s)
 
 
-class test_Input(object, driver):
+def test_mainPage_textFields():
 
-    def test_mainPage_textFields(self):
-        driver = self.driver
-        driver.get('http://127.0.0.1:5000/')
-        main = MainPage(driver)
+    main = MainPage(driver)
 
-        main.enter_motorKV('2400')
-        main.enter_battVolt('16.8')
-        main.enter_spur('18')
-        main.enter_pinion('54')
-        main.enter_fgr('3.92')
-        main.enter_wheelRadius('2.25')
-        main.click_calculate()
+    main.enter_motorKV('2400')
+    main.enter_battVolt('16.8')
+    main.enter_spur('18')
+    main.enter_pinion('54')
+    main.enter_fgr('3.92')
+    main.enter_wheelRadius('2.25')
+    time.sleep(1)
+    main.click_calculate()
 
 
-
+def test_input():
+    assert driver.find_element(By.ID, 'motorkV') == '2400'
 
 
 
@@ -40,9 +39,8 @@ class test_Input(object, driver):
 
 
 # fetch browser instance
-#driver.get('https://rcspeedestimator.herokuapp.com/')
+# #driver.get('https://rcspeedestimator.herokuapp.com/')
 #driver.get('http://127.0.0.1:5000/')
-
 
 
 '''# text_field id's = motorKV, battVolt, pinion, spur, fgr, wheelradius, submit-value=calculate
@@ -61,6 +59,6 @@ driver.find_element(By.ID, 'wheelradius').send_keys('2.75')
 # find the resulting speed from calculation that is displayed upon clicking 'calculate'
 calculated_speed = driver.find_element(By.CLASS_NAME, 'calculated').get_attribute('innerText')'''
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     if __name__ == '__main__':
-        pytest
+        pytest'''
